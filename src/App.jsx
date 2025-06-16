6e354993e0cfddcac1b825ca630a072bc3b48a62
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-16 13:27:32 +0100

    Add list markup

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,25 @@ import Form from './components/Form';
 import PackingList from './components/PackingList';
 import Stats from './components/Stats';
 
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: true },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Passports', quantity: 2, packed: false },
+  { id: 4, description: 'Socks', quantity: 12, packed: false },
+  { id: 5, description: 'Passports', quantity: 2, packed: false },
+  { id: 6, description: 'Socks', quantity: 12, packed: false },
+  { id: 7, description: 'Passports', quantity: 2, packed: false },
+  { id: 8, description: 'Socks', quantity: 12, packed: false },
+  { id: 9, description: 'Passports', quantity: 2, packed: false },
+  { id: 10, description: 'Socks', quantity: 12, packed: false },
+];
+
 function App() {
   return (
     <div className="app">
       <Logo />
       <Form />
-      <PackingList />
+      <PackingList items={initialItems} />
       <Stats />
     </div>
   );
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-function PackingList() {
-  return <div>PackingList</div>;
+// const initialItems = [
+//   { id: 1, description: 'Passports', quantity: 2, packed: false },
+//   { id: 2, description: 'Socks', quantity: 12, packed: false },
+// ];
+
+function Item({ item: { quantity, description, packed } }) {
+  return (
+    <li>
+      <span style={packed ? { textDecoration: 'line-through' } : {}}>
+        {quantity} {description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function PackingList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default PackingList;
