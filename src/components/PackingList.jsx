68d3626e6e7c296072b480a91aadb0e312325dca
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-17 16:18:55 +0100

    Cleanup

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-// const initialItems = [
-//   { id: 1, description: 'Passports', quantity: 2, packed: false },
-//   { id: 2, description: 'Socks', quantity: 12, packed: false },
-// ];
-
 function Item({ item: { quantity, description, packed } }) {
   return (
     <li>
